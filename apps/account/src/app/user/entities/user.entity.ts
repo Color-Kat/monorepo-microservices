dd087f63b2256b70abe7f2a6fbd968b3398cbc6d
Color-Kat <katik7979@gmail.com>
2025-01-27 20:49:10 +0300

    user entity courses management

--- apps/account/src/app/user/entities/user.entity.ts
+++ apps/account/src/app/user/entities/user.entity.ts
@@ -1,6 +1,7 @@
 import {
     IUser,
     IUserCourses,
+    PurchaseState,
     UserRole,
 } from '@monorepo-microservices/interfaces';
 import { compare, genSalt, hash } from 'bcryptjs';
@@ -23,12 +24,15 @@ export class UserEntity implements IUser {
         this.courses = user.courses;
     }
 
+    /**
+     * Return public profile object of the user info.
+     */
     public getPublicProfile() {
         return {
             email: this.email,
             role: this.role,
             displayName: this.displayName,
-        }
+        };
     }
 
     /**
@@ -50,9 +54,58 @@ export class UserEntity implements IUser {
         return compare(password, this.passwordHash);
     }
 
+    /**
+     * Update user data.
+     *
+     * @param data
+     */
     public updateProfile(data: Pick<IUser, 'displayName'>) {
         this.displayName = data.displayName;
 
         return this;
+    }
+
+    /**
+     * Add course to user courses.
+     *
+     * @param courseId
+     */
+    public addCourse(courseId: string) {
+        const existingCourse = this.courses.find(
+            (course) => course._id === courseId
+        );
+        if (existingCourse) throw new Error('Course already exists');
+
+        this.courses.push({
+            courseId,
+            purchaseState: PurchaseState.Started,
+        });
+
+        return this;
+    }
+
+    /**
+     * Delete course from user courses.
+     *
+     * @param courseId
+     */
+    public deleteCourse(courseId: string) {
+        this.courses = this.courses.filter((course) => course._id !== courseId);
+
+        return this;
+    }
+
+    /**
+     * Change course purchase state
+     *
+     * @param courseId
+     * @param state
+     */
+    public updateCoursePurchaceState(courseId: string, state: PurchaseState) {
+        this.courses = this.courses.map(course => {
+            if(course._id == courseId) course.purchaseState = state;
+
+            return course
+        })
     }
 }
